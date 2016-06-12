4fc35791a6f32da93f4e9cd28b2b9fc36845638a
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-11 23:23:03 -0600

    Add converters.

---
+++ Source/Cryptocurrency.Blockchain/Serialization/Converters/ByteArrayConverter.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace Cryptocurrency.Blockchain.Serialization.Converters
+{
+    public class ByteArrayConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(byte[]);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var encodedValue = reader.Value.ToString();
+            return ASCIIEncoding.ASCII.GetBytes(encodedValue);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var decodedValue = ASCIIEncoding.ASCII.GetChars((byte[])value).ToString();
+            serializer.Serialize(writer, decodedValue);
+        }
+    }
+}
---
+++ Source/Cryptocurrency.Blockchain/Serialization/Converters/DateTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿//using Newtonsoft.Json;
+//using System;
+
+//namespace Cryptocurrency.Blockchain.Serialization.Converters
+//{
+//    public class DateTimeConverter : JsonConverter
+//    {
+//        public override bool CanConvert(Type objectType)
+//        {
+//            return objectType == typeof(DateTime);
+//        }
+
+//        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+//        {
+//            return DateTimeOffset.FromUnixTimeSeconds((long)reader.Value).UtcDateTime;
+//        }
+
+//        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+//        {
+//            serializer.Serialize(writer, new DateTimeOffset((DateTime)value).ToUnixTimeSeconds());
+//        }
+//    }
+//}
